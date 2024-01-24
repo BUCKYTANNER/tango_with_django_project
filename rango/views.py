827c38516beb8cf8e291fb05c7e0a8c7a37589b7
Luke Latta <2787729l@student.gla.ac.uk>
2024-01-24 13:15:16 +0000

    Finished chapter 4, passes all tests

--- rango/views.py
+++ rango/views.py
@@ -9,11 +9,14 @@ def index(request):
    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
 
    # Return a rendered response to send to the client.
-   # We make use of the shortcut finction to make our lives easier.
+   # We make use of the shortcut function to make our lives easier.
    # Note that the first parameter is the template we wish to use.
 
    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+
+    context_dict = {'boldmessage': 'This tutorial has been put together by Luke Latta.'}
+
+    return render(request, 'rango/about.html', context=context_dict)
